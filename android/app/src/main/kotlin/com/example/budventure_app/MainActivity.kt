58b4257a7c0ad9e85282869641890a9ca16c2b2d
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-06-01 16:45:50 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/budventure_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.budventure_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/cart_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CartScreen'),
+      ),
+      body: Center(
+        child: Text('CartScreen'),
+      ),
+    );
+  }
+}
