b4c6797bc4fc958320953a9b46e26ba750ee09fe
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-06-02 14:01:49 +0530

    working on cart

---
+++ lib/controller/cart_controller.dart
@@ -0,0 +1,23 @@
+// import 'package:budventure_app/model/item_model.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+
+// class CartController extends GetxController {
+//   var _items = {}.obs;
+
+//   void addProduct(ItemModel itemModel) {
+//     if (_items.containsKey(itemModel)) {
+//       _items[itemModel] += 1;
+//     } else {
+//       _items[itemModel] = 1;
+//     }
+//   }
+//   //void removeProduct(){}
+
+//   //get product
+
+//   //get productSubtotal
+
+//   //get total
+
+// }
